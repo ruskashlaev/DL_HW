0dc6e7788e2e7e70f8b1d7a9b249df682c33e243
Ruslan <ruslan.kashlaev@gmail.com>
2026-07-29 01:29:22 +0300

    create some congif files

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -4,6 +4,18 @@ import torch
 
 
 def collate_fn(dataset_items: list[dict]):
+    """
+    Collate and pad fields in the dataset items.
+    Converts individual items into a batch.
+
+    Args:
+        dataset_items (list[dict]): list of objects from
+            dataset.__getitem__.
+    Returns:
+        result_batch (dict[Tensor]): dict, containing batch-version
+            of the tensors.
+    """
+
     K = 120000
     waveforms = []
     for elem in dataset_items:
